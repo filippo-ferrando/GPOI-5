83b9ac29b761b99347e6d13092196f6c94e69bea
filippoferrando <filippo.ferrando@itiscuneo.eu>
2022-03-30 09:22:27 +0200

    update

--- Progetto/app.py
+++ Progetto/app.py
@@ -3,7 +3,7 @@ import threading as thr
 from datetime import datetime
 import time
 import urllib.request
-
+import os
 
 from pirc522 import RFID
 import RPi.GPIO as GPIO
@@ -12,13 +12,13 @@ from gpiozero import Buzzer
 global offsetTagDict
 offsetTagDict = {}
 
-#GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 
 def connect(host='http://google.com'):
     try:
-        urllib.request.urlopen(host) #Python 3.x
+        urllib.request.urlopen(host)
         return True
     except:
         return False
@@ -40,18 +40,22 @@ class tagController(thr.Thread):
 
 class raspberry():
     def __init__(self):
-        self.api = "https://testgpoi2022.netsons.org/pres_auto/config/PHP/api_controllo_timbr.php"
+        self.api = "https://testgpoi2022.netsons.org/pres_auto/config/codice/api_controllo_timbr.php"
         self.password = "Rasp_rfid"
-        self.Rled = 6
+        self.Rled = 12
         GPIO.setmode(GPIO.BCM) 
         GPIO.setup(self.Rled,GPIO.OUT)
-        self.Gled = 5
+        self.Gled = 7
         GPIO.setup(self.Gled,GPIO.OUT)
         self.buzzer = Buzzer(26)
         self.rc522 = RFID()
+        if not connect():
+            print("Not connected to internet, exiting...")
+            os.sys.exit()
 
     def reader(self):
         global offsetTagDict
+        GPIO.setmode(GPIO.BOARD)
 
         print('In attesa del badge (per quittare, Ctrl + c): ')
 
@@ -76,29 +80,39 @@ class raspberry():
         if uid == 403:
             self.repeated_tag()
         else:
-            http = requests.post(self.api,data={'codMatr' : uid, 'password' : self.password})
+            http = requests.post(self.api,data={'codMatr' : uid, 'password' : self.password, 'modalita' : "modalita"})
             return http.text
 
     def bip(self, resp):
         if resp == "si":
+            GPIO.setmode(GPIO.BCM)
             GPIO.output(self.Gled, GPIO.HIGH)
+            GPIO.setmode(GPIO.BOARD)
             self.buzzer.on()
             time.sleep(0.3)
+            GPIO.setmode(GPIO.BOARD)
             self.buzzer.off()
             time.sleep(0.3)
             self.buzzer.on()
             time.sleep(0.3)
             self.buzzer.off()
+            GPIO.setmode(GPIO.BCM)
             GPIO.output(self.Gled, GPIO.LOW)
         elif resp == "no":
+            GPIO.setmode(GPIO.BCM)
             GPIO.output(self.Rled, GPIO.HIGH)
+            GPIO.setmode(GPIO.BOARD)
             self.buzzer.on()
             time.sleep(1)
             self.buzzer.off()
+            GPIO.setmode(GPIO.BCM)
             GPIO.output(self.Rled, GPIO.LOW)
+        elif resp == "password":
+            print("PASSWORD SBAGLIATA")
 
 
     def repeated_tag(self):
+        GPIO.setmode(GPIO.BCM)
         GPIO.output(self.Rled, GPIO.HIGH)
         time.sleep(1)
         GPIO.output(self.Rled, GPIO.LOW)
